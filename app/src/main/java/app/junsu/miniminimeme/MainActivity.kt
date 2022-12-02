4183fdf44cc5dfd3a26e4f18d21962f2c0d74867
Junsu <krpjs0508@gmail.com>
2022-12-03 03:42:55 +0900

    feat :: MainActivity bottomNavigationView logic

--- app/src/main/java/app/junsu/miniminimeme/MainActivity.kt
+++ app/src/main/java/app/junsu/miniminimeme/MainActivity.kt
@@ -1,11 +1,63 @@
 package app.junsu.miniminimeme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import app.junsu.miniminimeme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main,
+        )
+    }
+
+    private val homeFragment by lazy {
+        HomeFragment()
+    }
+
+    private val testFragment by lazy {
+        TestFragment()
+    }
+
+    private val userFragment by lazy {
+        UserFragment()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        with(binding.bottomNavigationViewMain) {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.bottom_navigation_view_main_home -> {
+                        changeFragment(homeFragment)
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.bottom_navigation_view_main_test -> {
+                        changeFragment(testFragment)
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.bottom_navigation_view_main_user -> {
+                        changeFragment(userFragment)
+                        return@setOnItemSelectedListener true
+                    }
+                    else -> {
+                        return@setOnItemSelectedListener false
+                    }
+                }
+            }
+            selectedItemId = R.id.bottom_navigation_view_main_home
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(
+            R.id.layout_main_container, fragment,
+        ).commitAllowingStateLoss()
     }
 }
